8a8cc77991b9cd936eb721491a15af8f1b5343de
sism1s <37706698+sism1s@users.noreply.github.com>
2020-07-28 00:22:16 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,32 +1,5 @@
 $(document).ready(function() {
     
-    $('.js--section-about').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-      offset: '60px;'
-    });
-
-
-    $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-              $('html,body').animate({
-                scrollTop: target.offset().top
-              }, 1000);
-              return false;
-            }
-          }
-        });
-      });
-
-
 
 VanillaTilt.init(document.querySelectorAll(".dan"),{
 
